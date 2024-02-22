dd268265d1a0a2aee4c6045b0b0f41123853c804
Milan Wesołowski <155390722+NalimWeso@users.noreply.github.com>
2024-02-22 15:17:51 +0100

    Update cartTest.js

--- projects/SuperSimpleDev/Amazon/tests/data/cartTest.js
+++ projects/SuperSimpleDev/Amazon/tests/data/cartTest.js
@@ -1,9 +1,11 @@
 import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe('Test suite: addToCart', () => {
-    it('Adds an existing product to the cart', () => {
+    beforeEach(() => {
         spyOn(localStorage, 'setItem');
+    });
 
+    it('Adds an existing product to the cart', () => {
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -22,8 +24,6 @@ describe('Test suite: addToCart', () => {
     });
 
     it('Adds a new product to the cart', () => {
-        spyOn(localStorage, 'setItem');
-
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
