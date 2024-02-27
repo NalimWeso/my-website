16a1b6e686e8e4986487688bead9b2cf91757472
Milan Wesołowski <155390722+NalimWeso@users.noreply.github.com>
2024-02-28 00:45:14 +0100

    Update swiperPortfolio.js

---
+++ scripts/swiperPortfolio.js
@@ -0,0 +1,45 @@
+let swiper;
+
+function adjustSlidesPerView() {
+
+    const swiperSettings = {
+        direction: 'horizontal',
+        loop: true,
+        autoHeight: true,
+        centeredSlides: true,
+        speed: 300,
+        spaceBetween: 10,
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: 30,
+            slideShadows: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+        autoplay: {
+            enabled: true,
+            disableOnInteraction: false,
+            delay: 3000
+        },
+        breakpoints: {
+            800: {
+                slidesPerView: 1,
+            },
+            801: {
+                slidesPerView: 2
+            }
+        },
+    };
+
+    if (swiper) {
+        swiper.destroy();
+    }
+
+    swiper = new Swiper('.swiper', swiperSettings);
+}
+
+adjustSlidesPerView();
+
+window.addEventListener('resize', adjustSlidesPerView);
