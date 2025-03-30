faac1be2efe152e96de2e5435cf2f7a3d3d1e117
Milan Wesołowski <milanweso@gmail.com>
2025-03-30 19:00:25 +0200

    Move project data to separate module

---
+++ content/projects.js
@@ -0,0 +1,84 @@
+export const personal = [
+    {
+        name: `My Website`,
+        GitHub: `href="https://github.com/NalimWeso/my-website"`,
+        Web: `href="https://nalimweso.com/"`,
+        description: `The unique core of my outer being.`,
+        technologies: `HTML | Sass | JavaScript`
+    },
+    {
+        name: `LifeBlueSnake`,
+        GitHub: `href="https://github.com/NalimWeso/lifebluesnake-electron-app"`,
+        Web: `class="inactive"`,
+        description: `Application for assigning tasks to employees.`,
+        technologies: `HTML | CSS | JavaScript | Electron`
+    },
+    {
+        name: `GeoSeq Checker`,
+        GitHub: `href="https://github.com/NalimWeso/geometric-sequence-checker"`,
+        Web: `href="https://nalimweso.github.io/geometric-sequence-checker/"`,
+        description: `Program for analyzing sets of numbers.`,
+        technologies: `HTML | CSS | JavaScript`
+    },
+
+    {
+        name: `Moving Day Counter`,
+        GitHub: `href="https://github.com/NalimWeso/moving-day-counter"`,
+        Web: `href="https://nalimweso.github.io/moving-day-counter/"`,
+        description: `Program for counting days to my moving out.`,
+        technologies: `HTML | CSS | JavaScript`
+    },
+    {
+        name: `Metal Gear Timeline`,
+        GitHub: `href="https://github.com/NalimWeso/metal-gear-timeline"`,
+        Web: `href="https://nalimweso.github.io/metal-gear-timeline/"`,
+        description: `The detailed chronology of Hideo Kojima's Metal Gear series.`,
+        technologies: `Technologies.`
+    },
+    {
+        name: `Netflix Replica`,
+        GitHub: `href="https://github.com/NalimWeso/service-replica"`,
+        Web: `href="https://nalimweso.github.io/service-replica/"`,
+        description: `Simulation of the Netflix series details panel.`,
+        technologies: `HTML | Sass | JavaScript`
+    },
+
+    {
+        name: `Hoodie Color Picker`,
+        GitHub: `href="https://github.com/NalimWeso/hoodie-color-picker"`,
+        Web: `href="https://nalimweso.github.io/hoodie-color-picker/"`,
+        description: `Program for choosing a random hoodie color.`,
+        technologies: `React | JavaScript | Sass`
+    },
+    {
+        name: `The Color Purple`,
+        GitHub: `href="https://github.com/NalimWeso/the-color-purple"`,
+        Web: `href="https://nalimweso.github.io/the-color-purple/"`,
+        description: `Fetch API data from TMDB for the 1985 and 2023 versions.`,
+        technologies: `React | JavaScript | PostCSS`
+    },
+    {
+        name: `Neon Cannes`,
+        GitHub: `href="https://github.com/NalimWeso/neon-cannes"`,
+        Web: `class="inactive"`,
+        description: `Application for managing a daily film routine.`,
+        technologies: `React | TypeScript | Electron | Tailwind`
+    },
+];
+
+export const collaborations = [
+    {
+        name: `Joblique`,
+        GitHub: `href="https://github.com/madina0801/joblique"`,
+        Web: `href="https://joblique.onrender.com/"`,
+        description: `Program for tracking job submissions.`,
+        technologies: `React | JavaScript | Tailwind`
+    },
+    {
+        name: `Public Witnessing Planner`,
+        GitHub: `href="https://github.com/teocrafters/pw-planner"`,
+        Web: `class="inactive"`,
+        description: `Organizer for Jehovah's Witnesses' public witnessing.`,
+        technologies: `Vue.js | TypeScript | Tailwind`
+    },
+];
--- content/script.js
+++ content/script.js
@@ -1,3 +1,6 @@
+import { personal, collaborations } from './projects.js';
+
+// Display Skills on Hover
 const gridItem = document.querySelectorAll('.grid-container div');
 const skillBoard = document.querySelector('.skill');
 
@@ -41,91 +44,6 @@ document.addEventListener('mouseover', (event) => {
 });
 
 // Render Projects
-const projectsPersonal = [
-    {
-        name: `My Website`,
-        GitHub: `href="https://github.com/NalimWeso/my-website"`,
-        Web: `href="https://nalimweso.com/"`,
-        description: `The unique core of my outer being.`,
-        technologies: `HTML | Sass | JavaScript`
-    },
-    {
-        name: `LifeBlueSnake`,
-        GitHub: `href="https://github.com/NalimWeso/lifebluesnake-electron-app"`,
-        Web: `class="inactive"`,
-        description: `Application for assigning tasks to employees.`,
-        technologies: `HTML | CSS | JavaScript | Electron`
-    },
-    {
-        name: `GeoSeq Checker`,
-        GitHub: `href="https://github.com/NalimWeso/geometric-sequence-checker"`,
-        Web: `href="https://nalimweso.github.io/geometric-sequence-checker/"`,
-        description: `Program for analyzing sets of numbers.`,
-        technologies: `HTML | CSS | JavaScript`
-    },
-
-    {
-        name: `Moving Day Counter`,
-        GitHub: `href="https://github.com/NalimWeso/moving-day-counter"`,
-        Web: `href="https://nalimweso.github.io/moving-day-counter/"`,
-        description: `Program for counting days to my moving out.`,
-        technologies: `HTML | CSS | JavaScript`
-    },
-    {
-        name: `Metal Gear Timeline`,
-        GitHub: `href="https://github.com/NalimWeso/metal-gear-timeline"`,
-        Web: `href="https://nalimweso.github.io/metal-gear-timeline/"`,
-        description: `The detailed chronology of Hideo Kojima's Metal Gear series.`,
-        technologies: `Technologies.`
-    },
-    {
-        name: `Netflix Replica`,
-        GitHub: `href="https://github.com/NalimWeso/service-replica"`,
-        Web: `href="https://nalimweso.github.io/service-replica/"`,
-        description: `Simulation of the Netflix series details panel.`,
-        technologies: `HTML | Sass | JavaScript`
-    },
-
-    {
-        name: `Hoodie Color Picker`,
-        GitHub: `href="https://github.com/NalimWeso/hoodie-color-picker"`,
-        Web: `href="https://nalimweso.github.io/hoodie-color-picker/"`,
-        description: `Program for choosing a random hoodie color.`,
-        technologies: `React | JavaScript | Sass`
-    },
-    {
-        name: `The Color Purple`,
-        GitHub: `href="https://github.com/NalimWeso/the-color-purple"`,
-        Web: `href="https://nalimweso.github.io/the-color-purple/"`,
-        description: `Fetch API data from TMDB for the 1985 and 2023 versions.`,
-        technologies: `React | JavaScript | PostCSS`
-    },
-    {
-        name: `Neon Cannes`,
-        GitHub: `href="https://github.com/NalimWeso/neon-cannes"`,
-        Web: `class="inactive"`,
-        description: `Application for managing a daily film routine.`,
-        technologies: `React | TypeScript | Electron | Tailwind`
-    },
-];
-
-const projectsCollaborations = [
-    {
-        name: `Joblique`,
-        GitHub: `href="https://github.com/madina0801/joblique"`,
-        Web: `href="https://joblique.onrender.com/"`,
-        description: `Program for tracking job submissions.`,
-        technologies: `React | JavaScript | Tailwind`
-    },
-    {
-        name: `Public Witnessing Planner`,
-        GitHub: `href="https://github.com/teocrafters/pw-planner"`,
-        Web: `class="inactive"`,
-        description: `Organizer for Jehovah's Witnesses' public witnessing.`,
-        technologies: `Vue.js | TypeScript | Tailwind`
-    },
-];
-
 const containerPersonal = document.querySelector('.projects-container.personal');
 const containerCollaborations = document.querySelector('.projects-container.collaborations');
 let fieldset = '';
@@ -176,10 +94,10 @@ function displayProjects(project) {
         ;
 }
 
-projectsPersonal.forEach(displayProjects);
+personal.forEach(displayProjects);
 containerPersonal.innerHTML += fieldset;
 fieldset = '';
 
-projectsCollaborations.forEach(displayProjects);
+collaborations.forEach(displayProjects);
 containerCollaborations.innerHTML += fieldset;
 fieldset = '';
